4efd66ad15bf79d92170be88425f52bc69ad1a24
Moges-Tesema <moges2421@gmail.com>
2024-12-29 11:36:31 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import react, { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-green-500 font-bold italic text-4x1'>hello world</div>
+  )
+}
+
+export default App
